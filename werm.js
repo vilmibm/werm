93e5977d04b0ba8bb58d9680feb4650b77d2f617
Nathaniel Smith <nathanielksmith@gmail.com>
2009-12-25 15:22:56 -0500

    js path search mostly working

--- werm.js
+++ werm.js
@@ -4,3 +4,32 @@ google.load("jqueryui", "1.7.2");
 var title = "werm";
 
 document.title = title;
+
+var ENTER = 13;
+var CL;
+var command;
+var output;
+
+function hello() {
+    return "hello world!";
+};
+
+google.setOnLoadCallback(function() {
+    CL = $("input.prompt");
+    CL.keypress(function(e) {
+        if ( e.which != ENTER ) { return; }
+        command = CL.attr("value");
+        // first try JS func
+        output = eval(command + "()");
+        if ( !output ) {
+            output = "Command not found."; // XXX this doesn't work
+        }
+        CL.attr("value",'');
+        $("span.prompt").before("<div>"+command+"</div>");
+        $("span.prompt").before("<div>"+output+"</div>");
+    });
+    CL.focus();
+});
+
+
+

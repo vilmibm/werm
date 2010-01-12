aabc9fc858eecb03c122fc40605f73810c309ec4
nathanielksmith <nathanielksmith@gmail.com>
2010-01-12 13:21:05 -0500

    support library loading

--- werm.js
+++ werm.js
@@ -20,38 +20,9 @@ var args;
 var output;
 
 var funcs = new Array();
-funcs["prompt_str"] = function (args) {
-    if ( args.length < 1 ) { return "Must supply a string"; }
-    PROMPT_STR = args.shift();
-    $("span.prompt").text(PROMPT_STR);
-    return "1";
-}
-
-funcs["hello"] = function (args) { return "hello world!"; };
-funcs["echo"]  = function (args) { return args.join(' '); };
-funcs["img"]   = function (args) {
-    if (args.length < 1) { return; }
-    return '<img src="'+args.shift()+'" />';
-}
-// XXX add support for nested operations
-funcs["math"]   = function (args) {
-    var op = args.shift();
-    var l  = new Number(args.shift());
-    var r  = new Number(args.shift());
-
-    var ops = new Array();
-    ops['+'] = function (l,r) { return l + r };
-    ops['-'] = function (l,r) { return l - r };
-    ops['*'] = function (l,r) { return l * r };
-    ops['/'] = function (l,r) { return l / r };
-
-    if ( ! ops[op] ) { return "Allowed operators: +, -, *, /"; }
-
-    return ops[op](l,r);
-}
 
 google.setOnLoadCallback(function() {
-    werm_load_lib('wermstd.js');
+    werm_load_lib('/werm/wermstd.js'); // load after jquery to provide jquery in lib
     $("span.prompt").text(PROMPT_STR);
     CL = $("input.prompt");
     CL.unbind();
@@ -87,6 +58,6 @@ function run_command(e) {
     }
 }
 
-function werm_load_lib() {
-    // XXX use jQuery to load specified lib file
+function werm_load_lib(lib) {
+    $("body").prepend('<script type="text/javascript" src="'+lib+'"></script>');
 }

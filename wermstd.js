b5aa51808540857ce1fd86542c5d420c372e7fcc
nathanielksmith <nathanielksmith@gmail.com>
2010-01-12 13:45:36 -0500

    stub tweet

--- wermstd.js
+++ wermstd.js
@@ -28,3 +28,20 @@ funcs["math"]   = function (args) {
 
     return ops[op](l,r);
 }
+
+funcs["tweet"] = function (args) {
+    if ( args.length != 3 ) { return "Usage: tweet <usr> <pass> <message>"; }
+    var user = args.shift();
+    var pass = args.shift();
+    var mess = args.shift();
+    if ( mess.length > 140 ) { return "Message too long." }
+
+    var url = 'http://twitter.com/statuses/update.xml'
+//    curl = 'curl -s -u %s:%s -d status="%s" %s' % (user,password,message,url)
+    $.ajax ({
+        url:        url
+        // XXX need base64 method beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'Basic ' + encodeBase64 (username+':'+'password') ); },
+        succes:     function(val) { alert(val); }
+        // XXX complete
+    });
+};
